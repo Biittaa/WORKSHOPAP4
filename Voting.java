ada978823ac7a9b94ad96113ed502e04acbf87e2
Biittaa <123160799+Biittaa@users.noreply.github.com>
2023-04-28 19:33:03 +0430

    voting some methods

--- Voting.java
+++ Voting.java
@@ -21,4 +21,37 @@ public class Voting {
     public void setQuestion(String question) {
         this.question = question;
     }
+    public ArrayList<String> getChoices() {
+        ArrayList<String> m = new ArrayList<>();
+        for(String s:choices.keySet()){
+            m.add(s);
+        }
+        return m;
+    }
+
+    public void createChoice(String choice) {
+        choices.put(choice, new HashSet<>());
+    }
+
+    public void vote(Person voter, ArrayList<String> voter_choices) {
+        if(!isAnonymous){
+            voters.add(voter);
+        }
+        for (String vChoices : voter_choices) {
+            String Date = LocalDate.now().toString();
+            Vote vote = new Vote(voter,Date);
+            choices.get(vChoices).add(vote);
+        }
+    }
+    public void vote(Person voter) {
+        if(!isAnonymous){
+            voters.add(voter);
+        }
+        Random r = new Random();
+        int m = r.nextInt(choices.size());
+        String Date = LocalDate.now().toString();
+        Vote vote = new Vote(voter,Date);
+        choices.get(m).add(vote);
+
+    }
 }
